f51a621123abaa8d8111f1d087a625a05b3e92b9
jl <jl.crobeddu@codeur.online>
2019-10-17 22:25:07 +0200

    setup footer

--- index.php
+++ index.php
@@ -490,7 +490,10 @@
 			</div>
 		</div>
 
+<?php get_footer(); ?>
 
+
+<!--
   <script src="https://kit.fontawesome.com/6fee70888d.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -501,4 +504,4 @@
 </script>
 </body>
 
-</html>
+</html> -->
